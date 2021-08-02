f79da1d653ef05a2ef06aa006c77fa0c3b5a31cf
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-08-02 12:05:59 +0100

    SalahTimes model

---
+++ SalahTimes/SalahTimes/SalahTimes.swift
@@ -0,0 +1,19 @@
+//
+//  SalahTimes.swift
+//  SalahTimes
+//
+//  Created by Suhayl Ahmed on 02/08/2021.
+//
+
+import Foundation
+
+public struct SalahTimes {
+    public let date: Date
+    public let location: String
+    public let fajr: Date
+    public let sunrise: Date
+    public let zuhr: Date
+    public let asr: Date
+    public let maghrib: Date
+    public let isha: Date
+}
