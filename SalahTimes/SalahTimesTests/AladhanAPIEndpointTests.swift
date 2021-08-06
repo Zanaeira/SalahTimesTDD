0a97219236a6b0741c5956cc5e3017545024bcfa
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-08-06 01:16:42 +0100

    Renamed timingsByCity to timingsByLocation and added URLQueryItems for city and country

--- SalahTimes/SalahTimesTests/AladhanAPIEndpointTests.swift
+++ SalahTimes/SalahTimesTests/AladhanAPIEndpointTests.swift
@@ -10,9 +10,13 @@ import SalahTimes
 
 struct AladhanAPIEndpoint {
     let path: String
+    let queryItems: [URLQueryItem]
     
-    static func timingsByCity(on date: Date) -> AladhanAPIEndpoint {
-        return AladhanAPIEndpoint(path: "/v1/timingsByCity/\(dateFormattedForAPIRequest(date))")
+    static func timingsByLocation(_ location: Location, on date: Date) -> AladhanAPIEndpoint {
+        return AladhanAPIEndpoint(path: "/v1/timingsByCity/\(dateFormattedForAPIRequest(date))", queryItems: [
+            URLQueryItem(name: "city", value: location.city),
+            URLQueryItem(name: "country", value: location.country)
+        ])
     }
     
     static func dateFormattedForAPIRequest(_ date: Date) -> String {
@@ -36,10 +40,28 @@ class AladhanAPIEndpointTests: XCTestCase {
     
     func test_timingsByCity_pathIsCorrectForDate() {
         let date = Date()
-        let sut = AladhanAPIEndpoint.timingsByCity(on: date)
+        let sut = AladhanAPIEndpoint.timingsByLocation(anyLocation(), on: date)
         let expectedPath = "/v1/timingsByCity/\(AladhanAPIEndpoint.dateFormattedForAPIRequest(date))"
         
         XCTAssertEqual(sut.path, expectedPath)
+    }
+    
+    func test_timingsByCity_queryItemsForCityAndCountryIncluded() {
+        let date = Date()
+        let location = Location(city: "London", country: "UK")
+        let sut = AladhanAPIEndpoint.timingsByLocation(location, on: date)
+        
+        let expectedQueryItems: [URLQueryItem] = [
+            URLQueryItem(name: "city", value: location.city),
+            URLQueryItem(name: "country", value: location.country)
+        ]
+        
+        XCTAssertEqual(sut.queryItems, expectedQueryItems)
+    }
+    
+    // MARK: - Helpers
+    private func anyLocation() -> Location {
+        return Location(city: "Anywhere", country: "In The World")
     }
     
 }
