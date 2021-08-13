818e28850246a0b70358ff6447de3ae197732522
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-08-13 13:44:48 +0100

    Setup for programmatic UIKit
    Removed Main.storyboard from project + Info.plist
    Setup scene delegate to load main view controller

---
+++ Prototype/Prototype/Controllers/SalahTimesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SalahTimesViewController.swift
+//  Prototype
+//
+//  Created by Suhayl Ahmed on 13/08/2021.
+//
+
+import UIKit
+
+final class SalahTimesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .systemTeal
+    }
+    
+}
