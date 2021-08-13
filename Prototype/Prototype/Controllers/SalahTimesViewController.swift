9a456b13285499bbf8f6f7cc19f10194539126de
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-08-13 17:06:46 +0100

    Added search bar to navigation bar

--- Prototype/Prototype/Controllers/SalahTimesViewController.swift
+++ Prototype/Prototype/Controllers/SalahTimesViewController.swift
@@ -9,14 +9,49 @@ import UIKit
 
 final class SalahTimesViewController: UIViewController {
     
+    private let searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupViews()
+        searchBar.delegate = self
+        
+        configureUI()
+        showSearchButton(true)
     }
     
-    private func setupViews() {
+    private func configureUI() {
         view.backgroundColor = .systemTeal
+        
+        title = "Salah Times"
+    }
+    
+    private func showSearchButton(_ shouldShow: Bool) {
+        shouldShow ? setupSearchButton() : hideSearchBar()
+    }
+    
+    private func setupSearchButton() {
+        navigationItem.titleView = nil
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar))
+        searchBar.showsCancelButton = true
+    }
+    
+    @objc private func showSearchBar() {
+        navigationItem.titleView = searchBar
+        navigationItem.leftBarButtonItem = nil
+        searchBar.becomeFirstResponder()
+    }
+    
+    private func hideSearchBar() {
+        showSearchButton(true)
+    }
+    
+}
+
+extension SalahTimesViewController: UISearchBarDelegate {
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        hideSearchBar()
     }
     
 }
