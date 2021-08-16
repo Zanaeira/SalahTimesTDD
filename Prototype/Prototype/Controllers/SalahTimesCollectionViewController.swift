4cda12f3d1bca24e9ab0abfe3bc61754f5fc882c
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-08-16 21:09:42 +0100

    Split Fajr start and end time to Fajr and Sunrise, removed Asr 1st Mithl

--- Prototype/Prototype/Controllers/SalahTimesCollectionViewController.swift
+++ Prototype/Prototype/Controllers/SalahTimesCollectionViewController.swift
@@ -43,9 +43,10 @@ extension SalahTimesCollectionViewController {
         let image: UIImage?
         
         static let stubs = [
-            Item(title: "Fajr", body: "03:26 - 05:48", image: UIImage(systemName: "sun.haze.fill")),
+            Item(title: "Fajr", body: "03:26", image: UIImage(systemName: "sun.haze.fill")),
+            Item(title: "Sunrise", body: "05:48", image: UIImage(systemName: "sunrise.fill")),
             Item(title: "Zuhr", body: "13:05", image: UIImage(systemName: "sun.max.fill")),
-            Item(title: "Asr", body: "17:03 | 18:06", image: UIImage(systemName: "sun.min.fill")),
+            Item(title: "Asr", body: "18:06", image: UIImage(systemName: "sun.min.fill")),
             Item(title: "Maghrib", body: "20:21", image: UIImage(systemName: "sunset.fill")),
             Item(title: "Isha", body: "22:31", image: UIImage(systemName: "moon.stars.fill")),
         ]
