c9831b937cad1bda014f4d5fd8a42d578d970757
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-08-20 13:23:39 +0100

    Removed extra trailing comma

--- Prototype/Prototype/Salah Times Screen/Models/Item.swift
+++ Prototype/Prototype/Salah Times Screen/Models/Item.swift
@@ -25,7 +25,7 @@ struct Item: Hashable {
             Item(title: "Zuhr", body: "13:05", image: UIImage(systemName: "sun.max.fill")),
             Item(title: "Asr", body: "17:03", image: UIImage(systemName: "sun.min.fill")),
             Item(title: "Maghrib", body: "20:21", image: UIImage(systemName: "sunset.fill")),
-            Item(title: "Isha", body: "22:31", image: UIImage(systemName: "moon.stars.fill")),
+            Item(title: "Isha", body: "22:31", image: UIImage(systemName: "moon.stars.fill"))
         ],
         [
             Item(title: "Fajr", body: "03:29", image: UIImage(systemName: "sun.haze.fill")),
@@ -33,7 +33,7 @@ struct Item: Hashable {
             Item(title: "Zuhr", body: "13:05", image: UIImage(systemName: "sun.max.fill")),
             Item(title: "Asr", body: "17:02", image: UIImage(systemName: "sun.min.fill")),
             Item(title: "Maghrib", body: "20:19", image: UIImage(systemName: "sunset.fill")),
-            Item(title: "Isha", body: "22:28", image: UIImage(systemName: "moon.stars.fill")),
+            Item(title: "Isha", body: "22:28", image: UIImage(systemName: "moon.stars.fill"))
         ],
         [
             Item(title: "Fajr", body: "03:32", image: UIImage(systemName: "sun.haze.fill")),
@@ -41,7 +41,7 @@ struct Item: Hashable {
             Item(title: "Zuhr", body: "13:04", image: UIImage(systemName: "sun.max.fill")),
             Item(title: "Asr", body: "17:01", image: UIImage(systemName: "sun.min.fill")),
             Item(title: "Maghrib", body: "20:17", image: UIImage(systemName: "sunset.fill")),
-            Item(title: "Isha", body: "22:25", image: UIImage(systemName: "moon.stars.fill")),
+            Item(title: "Isha", body: "22:25", image: UIImage(systemName: "moon.stars.fill"))
         ],
         [
             Item(title: "Fajr", body: "03:35", image: UIImage(systemName: "sun.haze.fill")),
@@ -49,7 +49,7 @@ struct Item: Hashable {
             Item(title: "Zuhr", body: "13:04", image: UIImage(systemName: "sun.max.fill")),
             Item(title: "Asr", body: "17:00", image: UIImage(systemName: "sun.min.fill")),
             Item(title: "Maghrib", body: "20:15", image: UIImage(systemName: "sunset.fill")),
-            Item(title: "Isha", body: "22:22", image: UIImage(systemName: "moon.stars.fill")),
+            Item(title: "Isha", body: "22:22", image: UIImage(systemName: "moon.stars.fill"))
         ],
         [
             Item(title: "Fajr", body: "03:37", image: UIImage(systemName: "sun.haze.fill")),
@@ -57,7 +57,7 @@ struct Item: Hashable {
             Item(title: "Zuhr", body: "13:04", image: UIImage(systemName: "sun.max.fill")),
             Item(title: "Asr", body: "16:59", image: UIImage(systemName: "sun.min.fill")),
             Item(title: "Maghrib", body: "20:13", image: UIImage(systemName: "sunset.fill")),
-            Item(title: "Isha", body: "22:19", image: UIImage(systemName: "moon.stars.fill")),
+            Item(title: "Isha", body: "22:19", image: UIImage(systemName: "moon.stars.fill"))
         ]
     ]
     
