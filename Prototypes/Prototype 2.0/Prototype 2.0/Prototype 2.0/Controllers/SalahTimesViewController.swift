3899c0ede7ce6e94c9586d3beea0c045b5eb4761
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-12-08 19:33:43 +0000

    Added SearchController

--- Prototypes/Prototype 2.0/Prototype 2.0/Prototype 2.0/Controllers/SalahTimesViewController.swift
+++ Prototypes/Prototype 2.0/Prototype 2.0/Prototype 2.0/Controllers/SalahTimesViewController.swift
@@ -13,6 +13,9 @@ final class SalahTimesViewController: UIViewController {
         fatalError("Not implemented")
     }
     
+    private let searchController = UISearchController()
+    private var location: String?
+    
     private let salahTimesLoader: SalahTimesLoader
     
     init(salahTimesLoader: SalahTimesLoader) {
@@ -25,10 +28,44 @@ final class SalahTimesViewController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        setupSearchBar()
     }
     
     private func configureUI() {
         view.backgroundColor = .systemTeal
+    }
+    
+    private func refresh() {
+        if let location = location {
+            print("Getting Salah Times for: \(location)")
+        }
+    }
+    
+}
+
+// MARK: - Search Controller functionality
+extension SalahTimesViewController: UISearchBarDelegate {
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        searchController.searchBar.placeholder = "Enter location. E.g. London"
+        
+        searchController.searchBar.delegate = self
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc
+    private func dismissKeyboard() {
+        searchController.isActive = false
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        location = searchBar.text
+        searchController.isActive = false
+        refresh()
     }
     
 }
