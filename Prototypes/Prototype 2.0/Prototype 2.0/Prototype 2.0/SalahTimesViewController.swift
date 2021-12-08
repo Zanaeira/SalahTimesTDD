ef06b102ae6f6411c3ac134ad741e5a8b545b6bc
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-12-08 16:05:54 +0000

    Added base project template for Prototype 2.0
    Set up for programmatic UIKit, no storyboards.

---
+++ Prototypes/Prototype 2.0/Prototype 2.0/Prototype 2.0/SalahTimesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SalahTimesViewController.swift
+//  Prototype 2.0
+//
+//  Created by Suhayl Ahmed on 08/12/2021.
+//
+
+import UIKit
+
+final class SalahTimesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .systemTeal
+    }
+    
+}
---
+++ Prototypes/Prototype 2.0/Prototype 2.0/Prototype 2.0/SceneDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  SceneDelegate.swift
+//  Prototype 2.0
+//
+//  Created by Suhayl Ahmed on 08/12/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let salahTimesViewController = SalahTimesViewController()
+        salahTimesViewController.title = "Salāh Times"
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = NavigationController(rootViewController: salahTimesViewController).controller
+        window?.makeKeyAndVisible()
+    }
+    
+}
+
+private final class NavigationController {
+    
+    let controller: UINavigationController
+    
+    init(rootViewController viewController: UIViewController) {
+        controller = UINavigationController(rootViewController: viewController)
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        if #available(iOS 15.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .systemGray6
+            
+            controller.navigationBar.standardAppearance = appearance
+            controller.navigationBar.scrollEdgeAppearance = appearance
+        }
+    }
+    
+}
