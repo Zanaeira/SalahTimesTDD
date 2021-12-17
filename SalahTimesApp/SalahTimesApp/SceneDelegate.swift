dadff7f7c583ce2021ce5ff4d9e52867e408ff39
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2021-12-17 00:29:14 +0000

    Implemented caching strategy - using URLCache so that data can be loaded offline

--- SalahTimesApp/SalahTimesApp/SceneDelegate.swift
+++ SalahTimesApp/SalahTimesApp/SceneDelegate.swift
@@ -15,13 +15,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
-        let client = URLSessionHTTPClient()
+        let config = URLSessionConfiguration.default
+        config.urlCache = makeCache()
+        config.requestCachePolicy = .returnCacheDataElseLoad
+                
+        let session = URLSession(configuration: config)
+        
+        let client = URLSessionHTTPClient(session: session)
         let tabBarController = MainTabBarViewController(client: client)
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
+    }
+    
+    private func makeCache() -> URLCache {
+        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        let diskCacheURL = cachesURL.appendingPathComponent("DownloadCache")
+        
+        return URLCache(memoryCapacity: 10 * 1024 * 1024, diskCapacity: 100 * 1024 * 1024, directory: diskCacheURL)
     }
     
 }
