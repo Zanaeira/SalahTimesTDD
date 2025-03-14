d0ddc8e6396ee9728efc1462c3bf7c6e43e2f7eb
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-03-14 01:18:58 +0000

    Add tabs

---
+++ SalahTimes/SalahTimesSwiftUI/CalendarScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarScreen.swift
+//  SalahTimesSwiftUI
+//
+//  Created by Suhayl Ahmed on 14/03/2025.
+//
+
+import SwiftUI
+
+public struct CalendarScreen: View {
+
+	public init() {}
+
+	public var body: some View {
+		Label("Calendar", systemImage: "calendar")
+	}
+
+}
