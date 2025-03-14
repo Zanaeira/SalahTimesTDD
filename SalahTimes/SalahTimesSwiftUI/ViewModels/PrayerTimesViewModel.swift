b1f5382e4de93e29457a7c75819a05bcff1e20cb
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-03-14 01:24:07 +0000

    Extract ViewModel and group screens etc.

--- SalahTimes/SalahTimesSwiftUI/ViewModels/PrayerTimesViewModel.swift
+++ SalahTimes/SalahTimesSwiftUI/ViewModels/PrayerTimesViewModel.swift
@@ -1,31 +1,12 @@
 //
-//  PrayerTimesScreen.swift
+//  PrayerTimesViewModel.swift
 //  SalahTimesSwiftUI
 //
-//  Created by Suhayl Ahmed on 13/03/2025.
+//  Created by Suhayl Ahmed on 14/03/2025.
 //
 
-import SwiftUI
-import SalahTimes
-
-public struct PrayerTimesScreen: View {
-
-	public init() {}
-
-	public var body: some View {
-		VStack {
-			Text(viewModel.date, format: .dateTime.weekday().day().month().year())
-			Text(viewModel.label)
-		}
-		.task { await viewModel.load() }
-		.frame(maxWidth: .infinity, maxHeight: .infinity)
-		.background(BackgroundView())
-		.ignoresSafeArea()
-	}
-
-	@StateObject private var viewModel: PrayerTimesViewModel = .init()
-
-}
+import Foundation
+internal import SalahTimes
 
 @MainActor
 class PrayerTimesViewModel: ObservableObject {
