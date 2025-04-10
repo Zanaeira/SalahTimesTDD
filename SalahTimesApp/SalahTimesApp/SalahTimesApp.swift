befb346a1668481474ba6c3921bc8b3619008bca
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-04-10 23:34:26 +0100

    Register UserDefaults on appear

--- SalahTimesApp/SalahTimesApp/SalahTimesApp.swift
+++ SalahTimesApp/SalahTimesApp/SalahTimesApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SalahTimes
 import SalahTimesSwiftUI
 
 @main
@@ -20,7 +21,39 @@ struct SalahTimesApp: App {
 				CalendarScreen()
 					.tabItem { Label("Calendar", systemImage: "calendar") }
 			}
+			.onAppear { registerDefaults() }
 		}
+	}
+
+	private func registerDefaults() {
+		let suiteDefaults = UserDefaults.standard
+		suiteDefaults.register(defaults: [
+			"suiteNames": ["253FAFE2-96C6-42AF-8908-33DA339BD6C7"]
+		])
+
+		let suiteNames = suiteDefaults.stringArray(forKey: "suiteNames") ?? ["253FAFE2-96C6-42AF-8908-33DA339BD6C7"]
+		for suiteName in suiteNames {
+			if let defaults = UserDefaults(suiteName: suiteName) {
+				registerSalahTimes(defaults, forDefaultLocation: "London")
+			}
+		}
+	}
+
+	private func registerSalahTimes(_ suite: UserDefaults, forDefaultLocation location: String) {
+		suite.register(defaults: [
+			"Mithl": 2,
+			"Location": location
+		])
+
+		if let fajrIshaMethod = getEncodedFajrIshaMethod() {
+			suite.register(defaults: [
+				"FajrIsha": fajrIshaMethod
+			])
+		}
+	}
+
+	private func getEncodedFajrIshaMethod() -> Data? {
+		try? JSONEncoder().encode(AladhanAPIEndpoint.Method.custom(methodSettings: .init(fajrAngle: 12.0, maghribAngle: nil, ishaAngle: 12.0)))
 	}
 
 }
