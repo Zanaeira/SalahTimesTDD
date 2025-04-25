f472f43dcf3d0a5880a8f9feb713e9dc9288179a
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-04-26 00:22:25 +0100

    Extract types to separate files

---
+++ SalahTimes/SalahTimesSwiftUI/Screens/Helpers/Styling.swift
@@ -0,0 +1,28 @@
+//
+//  Styling.swift
+//  SalahTimesSwiftUI
+//
+//  Created by Suhayl Ahmed on 26/04/2025.
+//
+
+import SwiftUI
+
+struct SalahOverviewStyle: GroupBoxStyle {
+	func makeBody(configuration: Configuration) -> some View {
+		VStack(spacing: 8) {
+			configuration.label
+			configuration.content
+		}
+		.padding(16)
+		.frame(maxWidth: .infinity)
+		.background {
+			RoundedRectangle(cornerRadius: 16)
+				.fill(.teal.opacity(0.4))
+				.stroke(.white, lineWidth: 1)
+		}
+	}
+}
+
+extension GroupBoxStyle where Self == SalahOverviewStyle {
+	static var salahOverview: Self { .init() }
+}
