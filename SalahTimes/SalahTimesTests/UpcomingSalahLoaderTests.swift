a917377f39f5c5438eb78c2f2992b03cd6324428
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-05-02 00:56:55 +0100

    Fix formatting

--- SalahTimes/SalahTimesTests/UpcomingSalahLoaderTests.swift
+++ SalahTimes/SalahTimesTests/UpcomingSalahLoaderTests.swift
@@ -36,25 +36,25 @@ public final class UpcomingSalahLoader {
 final class UpcomingSalahLoaderTests: XCTestCase {
 
 	func test_load_deliversConnectivityErrorOnHTTPClientError() {
-			let (sut, httpClient, endpointSpy) = makeSUT()
+		let (sut, httpClient, endpointSpy) = makeSUT()
 
-			expect(sut, toCompleteWith: .failure(.connectivity), using: endpointSpy) {
-					let httpClientError = NSError(domain: "Error", code: 0)
-					httpClient.complete(with: httpClientError)
-			}
+		expect(sut, toCompleteWith: .failure(.connectivity), using: endpointSpy) {
+			let httpClientError = NSError(domain: "Error", code: 0)
+			httpClient.complete(with: httpClientError)
+		}
 	}
 
 	func test_load_deliversInvalidDataErrorOnNon200HTTPResponse() {
-			let (sut, httpClient, endpointSpy) = makeSUT()
+		let (sut, httpClient, endpointSpy) = makeSUT()
 
-			let sampleStatusCodes = [199, 201, 300, 400, 500]
+		let sampleStatusCodes = [199, 201, 300, 400, 500]
 
-			sampleStatusCodes.enumerated().forEach { index, code in
-					expect(sut, toCompleteWith: .failure(.invalidData), using: endpointSpy) {
-						let upcomingSalah = ["Fajr": "2025-04-30T03:38:00+01:00"]
-						httpClient.complete(withStatusCode: code, data: makeUpcomingSalahJSON(upcomingSalah), at: index)
-					}
+		sampleStatusCodes.enumerated().forEach { index, code in
+			expect(sut, toCompleteWith: .failure(.invalidData), using: endpointSpy) {
+				let upcomingSalah = ["Fajr": "2025-04-30T03:38:00+01:00"]
+				httpClient.complete(withStatusCode: code, data: makeUpcomingSalahJSON(upcomingSalah), at: index)
 			}
+		}
 	}
 
 	private func makeUpcomingSalahJSON(_ timings: [String: String]) -> Data {
@@ -66,55 +66,55 @@ final class UpcomingSalahLoaderTests: XCTestCase {
 	// MARK: - Helpers
 
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (upcomingSalahTimesLoader: UpcomingSalahLoader, httpClient: HTTPClientSpy, endpoint: Endpoint) {
-			let httpClient = HTTPClientSpy()
-			let endpointSpy = EndpointSpy.make()
-			let sut = UpcomingSalahLoader(client: httpClient)
-			trackForMemoryLeaks(sut, file: file, line: line)
-			trackForMemoryLeaks(httpClient, file: file, line: line)
+		let httpClient = HTTPClientSpy()
+		let endpointSpy = EndpointSpy.make()
+		let sut = UpcomingSalahLoader(client: httpClient)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		trackForMemoryLeaks(httpClient, file: file, line: line)
 
-			return (sut, httpClient, endpointSpy)
+		return (sut, httpClient, endpointSpy)
 	}
 
 	private func expect(_ sut: UpcomingSalahLoader, toCompleteWith result: UpcomingSalahLoader.Result, using endpoint: Endpoint, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
-			var capturedResults = [UpcomingSalahLoader.Result]()
-			sut.load(from: endpoint) {
-					capturedResults.append($0)
-			}
+		var capturedResults = [UpcomingSalahLoader.Result]()
+		sut.load(from: endpoint) {
+			capturedResults.append($0)
+		}
 
-			action()
+		action()
 
-			XCTAssertEqual(capturedResults, [result], file: file, line: line)
+		XCTAssertEqual(capturedResults, [result], file: file, line: line)
 	}
 
 	private final class HTTPClientSpy: HTTPClient {
-			typealias Completion = (Result<(Data, HTTPURLResponse), Error>) -> Void
+		typealias Completion = (Result<(Data, HTTPURLResponse), Error>) -> Void
 
-			private var urlCompletions = [(url: URL, completion: Completion)]()
+		private var urlCompletions = [(url: URL, completion: Completion)]()
 
-			func get(from url: URL, completion: @escaping Completion) {
-					urlCompletions.append((url, completion))
-			}
+		func get(from url: URL, completion: @escaping Completion) {
+			urlCompletions.append((url, completion))
+		}
 
-			func complete(with error: Error, at index: Int = 0) {
-					urlCompletions[index].completion(.failure(error))
-			}
+		func complete(with error: Error, at index: Int = 0) {
+			urlCompletions[index].completion(.failure(error))
+		}
 
-			func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-					let response = HTTPURLResponse(url: urlCompletions[index].url, statusCode: code, httpVersion: nil, headerFields: nil)!
-					urlCompletions[index].completion(.success((data, response)))
-			}
+		func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+			let response = HTTPURLResponse(url: urlCompletions[index].url, statusCode: code, httpVersion: nil, headerFields: nil)!
+			urlCompletions[index].completion(.success((data, response)))
+		}
 	}
 
 	private struct EndpointSpy: Endpoint {
-			let path: String
-			let queryItems: [URLQueryItem]
-			var url: URL {
-					URL(string: "https://a-given-url.com")!
-			}
+		let path: String
+		let queryItems: [URLQueryItem]
+		var url: URL {
+			URL(string: "https://a-given-url.com")!
+		}
 
-			static func make() -> EndpointSpy {
-					.init(path: "", queryItems: [])
-			}
+		static func make() -> EndpointSpy {
+			.init(path: "", queryItems: [])
+		}
 	}
 
 }
