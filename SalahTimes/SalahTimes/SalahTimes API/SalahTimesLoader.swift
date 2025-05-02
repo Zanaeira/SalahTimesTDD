c3988c5a774bc76f2abefc4eb8884a4de0300cef
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-05-03 00:36:50 +0100

    Fix formatting and make Mappers match structure / style

--- SalahTimes/SalahTimes/SalahTimes API/SalahTimesLoader.swift
+++ SalahTimes/SalahTimes/SalahTimes API/SalahTimesLoader.swift
@@ -8,31 +8,30 @@
 import Foundation
 
 public final class SalahTimesLoader: TimesLoader {
-    
-    public typealias Result = TimesLoader.Result
-    
-    private let client: HTTPClient
-    
-    public init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    public func loadTimes(from endpoint: Endpoint, completion: @escaping (Result) -> Void) {
-        client.get(from: endpoint.url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                guard let salahTimes = try? SalahTimesMapper.map(data, response) else {
-                    return completion(.failure(.invalidData))
-                }
-                
-                completion(.success(salahTimes))
-            case .failure:
-                completion(.failure(.connectivity))
-            }
-        }
-    }
+	public typealias Result = TimesLoader.Result
+
+	private let client: HTTPClient
+
+	public init(client: HTTPClient) {
+		self.client = client
+	}
+
+	public func loadTimes(from endpoint: Endpoint, completion: @escaping (Result) -> Void) {
+		client.get(from: endpoint.url) { [weak self] result in
+			guard self != nil else { return }
+
+			switch result {
+			case let .success((data, response)):
+				guard let salahTimes = try? SalahTimesMapper.map(data, response) else {
+					return completion(.failure(.invalidData))
+				}
+
+				completion(.success(salahTimes))
+			case .failure:
+				completion(.failure(.connectivity))
+			}
+		}
+	}
 
 	public func loadTimes(from endpoint: any Endpoint) async -> Result {
 		await withCheckedContinuation { continuation in
