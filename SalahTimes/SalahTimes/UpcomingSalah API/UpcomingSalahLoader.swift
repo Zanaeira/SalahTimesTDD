625650ad8a83e79881a8703494b64960a18e46d8
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-05-03 17:06:32 +0100

    Make `UpcomingSalahLoader` use the generic `RemoteLoader`

--- SalahTimes/SalahTimes/UpcomingSalah API/UpcomingSalahLoader.swift
+++ SalahTimes/SalahTimes/UpcomingSalah API/UpcomingSalahLoader.swift
@@ -7,36 +7,10 @@
 
 import Foundation
 
-public final class UpcomingSalahLoader {
-	public typealias Result = Swift.Result<UpcomingSalah, LoaderError>
+public typealias UpcomingSalahLoader = RemoteLoader<UpcomingSalah>
 
-	private let client: HTTPClient
-
-	public init(client: HTTPClient) {
-		self.client = client
-	}
-
-	public func load(from endpoint: Endpoint, completion: @escaping (Result) -> Void) {
-		client.get(from: endpoint.url) { [weak self] result in
-			guard self != nil else { return }
-
-			switch result {
-			case .success(let (date, response)):
-				guard let upcomingSalah = try? UpcomingSalahMapper.map(date, response) else {
-					return completion(.failure(.invalidData))
-				}
-				completion(.success(upcomingSalah))
-			case .failure:
-				completion(.failure(.connectivity))
-			}
-		}
-	}
-
-	public func load(from endpoint: any Endpoint) async -> Result {
-		await withCheckedContinuation { continuation in
-			load(from: endpoint) { result in
-				continuation.resume(returning: result)
-			}
-		}
+public extension UpcomingSalahLoader {
+	convenience init(client: HTTPClient) {
+		self.init(client: client, mapper: UpcomingSalahMapper.map)
 	}
 }
