549625f40cfd7051ef4eb5ca52f2ae0d1a986128
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-05-03 16:24:39 +0100

    Duplicate SalahTimesLoader as RemoteLoader

---
+++ SalahTimes/SalahTimes/Shared API/RemoteLoader.swift
@@ -0,0 +1,44 @@
+//
+//  RemoteLoader.swift
+//  SalahTimes
+//
+//  Created by Suhayl Ahmed on 03/05/2025.
+//
+
+import Foundation
+
+public final class RemoteLoader: TimesLoader {
+	public typealias Result = TimesLoader.Result
+
+	private let client: HTTPClient
+
+	public init(client: HTTPClient) {
+		self.client = client
+	}
+
+	public func load(from endpoint: Endpoint, completion: @escaping (Result) -> Void) {
+		client.get(from: endpoint.url) { [weak self] result in
+			guard self != nil else { return }
+
+			switch result {
+			case let .success((data, response)):
+				guard let salahTimes = try? SalahTimesMapper.map(data, response) else {
+					return completion(.failure(.invalidData))
+				}
+				completion(.success(salahTimes))
+			case .failure:
+				completion(.failure(.connectivity))
+			}
+		}
+	}
+
+	public func load(from endpoint: any Endpoint) async -> Result {
+		await withCheckedContinuation { continuation in
+			load(from: endpoint) { result in
+				continuation.resume(returning: result)
+			}
+		}
+	}
+
+}
+
