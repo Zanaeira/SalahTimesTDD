f878b4e8d4635209465aa38e19285df3f0d0331c
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-05-03 16:56:57 +0100

    Remove duplication and replace SalahTimesLoader with generic loader

--- SalahTimes/SalahTimes/SalahTimes API/SalahTimesLoader.swift
+++ SalahTimes/SalahTimes/SalahTimes API/SalahTimesLoader.swift
@@ -7,37 +7,10 @@
 
 import Foundation
 
-public final class SalahTimesLoader: TimesLoader {
-	public typealias Result = TimesLoader.Result
+public typealias SalahTimesLoader = RemoteLoader<SalahTimes>
 
-	private let client: HTTPClient
-
-	public init(client: HTTPClient) {
-		self.client = client
+public extension SalahTimesLoader {
+	convenience init(client: HTTPClient) {
+		self.init(client: client, mapper: SalahTimesMapper.map)
 	}
-
-	public func load(from endpoint: Endpoint, completion: @escaping (Result) -> Void) {
-		client.get(from: endpoint.url) { [weak self] result in
-			guard self != nil else { return }
-
-			switch result {
-			case let .success((data, response)):
-				guard let salahTimes = try? SalahTimesMapper.map(data, response) else {
-					return completion(.failure(.invalidData))
-				}
-				completion(.success(salahTimes))
-			case .failure:
-				completion(.failure(.connectivity))
-			}
-		}
-	}
-
-	public func load(from endpoint: any Endpoint) async -> Result {
-		await withCheckedContinuation { continuation in
-			load(from: endpoint) { result in
-				continuation.resume(returning: result)
-			}
-		}
-	}
-
 }
