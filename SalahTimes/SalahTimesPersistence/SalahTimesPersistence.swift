87d8b200cf2f2abc070b4a1057e70534d5cdd434
Suhayl Ahmed <suhaylibnbanu@gmail.com>
2025-10-23 23:24:49 +0100

    Add persistance target

---
+++ SalahTimes/SalahTimesPersistence/SalahTimesPersistence.swift
@@ -0,0 +1,9 @@
+//
+//  SalahTimesPersistence.swift
+//  SalahTimesPersistence
+//
+//  Created by Suhayl Ahmed on 23/10/2025.
+//
+
+import Foundation
+
---
+++ SalahTimes/SalahTimesPersistenceTests/SalahTimesPersistenceTests.swift
@@ -0,0 +1,13 @@
+//
+//  SalahTimesPersistenceTests.swift
+//  SalahTimesPersistenceTests
+//
+//  Created by Suhayl Ahmed on 23/10/2025.
+//
+
+import XCTest
+import SalahTimesPersistence
+
+final class SalahTimesPersistenceTests: XCTestCase {
+
+}
